d1b92f77b391e48e52914206f54b62517413b13b
SeoSoojin <brightGowon.hye@gmail.com>
2021-12-27 01:17:05 -0300

    new errors added

--- pkg/domain/models/errors.ts
+++ pkg/domain/models/errors.ts
@@ -1,4 +1,7 @@
 const errInvalidId = new Error('Invalid id');
 const errEmptyField = new Error('Empty field');
+const errNotFound = new Error('Not found');
+const errEmptyAuthor = new Error('Empty author');
+const errInvalidAuthorId = new Error('Invalid author id');
 
-export { errInvalidId, errEmptyField };
+export { errInvalidId, errEmptyField, errNotFound, errEmptyAuthor, errInvalidAuthorId };
