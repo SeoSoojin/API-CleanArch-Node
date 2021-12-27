3b1fd756a4bba82acca5c3be43882418362ec623
SeoSoojin <brightGowon.hye@gmail.com>
2021-12-27 01:18:46 -0300

    app created

---
+++ index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import * as bodyParser from "body-parser";
+import { routes } from './routes';
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.use('/', routes);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}/`);
+});
