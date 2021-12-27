850d4e2a1bc5ccbe95526f56f289bbaea98f8c3a
SeoSoojin <brightGowon.hye@gmail.com>
2021-12-27 01:17:58 -0300

    now rejects on various errors

--- pkg/domain/services/postWriter.ts
+++ pkg/domain/services/postWriter.ts
@@ -1,4 +1,5 @@
 import { PostWriter } from "../interfaces/interfaces";
+import { errEmptyAuthor, errEmptyField, errInvalidAuthorId, errInvalidId } from "../models/errors";
 import { Post } from "../models/post";
 
 class PostWriterService  { 
@@ -13,6 +14,46 @@ class PostWriterService  {
 
     insertPost(post: Post): Promise<null | Error> {
         
+        if (!post.author) {
+
+            return new Promise((resolve, reject) => {
+                reject(errEmptyAuthor)
+            })
+
+        }
+
+        if (!post.author.id || post.author.id <= 0) {
+
+            return new Promise((resolve, reject) => {
+                reject(errInvalidAuthorId)
+            })
+
+        }
+
+        if (!post.title || post.title.length === 0) {
+
+            return new Promise((resolve, reject) => {
+                reject(errEmptyField)
+            })
+
+        }
+
+        if (!post.description || post.description.length === 0) {
+
+            return new Promise((resolve, reject) => {
+                reject(errEmptyField)
+            })
+
+        }
+
+        if (!post.category || post.category.length === 0) {
+
+            return new Promise((resolve, reject) => {
+                reject(errEmptyField)
+            })
+
+        }
+        
         return this.postWriter.insertPost(post)
     
     }
@@ -20,10 +61,51 @@ class PostWriterService  {
     updatePost(id: number, post: Post): Promise<null | Error> {
 
         if (id <= 0) {
-            return new Promise((reject) => {
+            return new Promise((resolve, reject) => {
                 reject(new Error("invalid id, should be greater than 0"))
             })
         }
+
+        if (!post.author) {
+
+            return new Promise((resolve, reject) => {
+                reject(errEmptyAuthor)
+            })
+
+        }
+
+        if (!post.author.id || post.author.id <= 0) {
+
+            return new Promise((resolve, reject) => {
+                reject(errInvalidAuthorId)
+            })
+
+        }
+
+        if (!post.title || post.title.length === 0) {
+
+            return new Promise((resolve, reject) => {
+                reject(errEmptyField)
+            })
+
+        }
+
+        if (!post.description || post.description.length === 0) {
+
+            return new Promise((resolve, reject) => {
+                reject(errEmptyField)
+            })
+
+        }
+
+        if (!post.category || post.category.length === 0) {
+
+            return new Promise((resolve, reject) => {
+                reject(errEmptyField)
+            })
+
+        }
+
         return this.postWriter.updatePost(id, post)
 
     }
